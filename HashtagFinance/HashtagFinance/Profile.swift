da200728bf1d36bc956b8fc3c74771423aba9883
Bondzio, Kevin M (bondz001) <bondz001@maclab-112.uwp.edu>
2016-04-19 16:10:15 -0500

    added User as a Swift Class

---
+++ HashtagFinance/HashtagFinance/Profile.swift
@@ -0,0 +1,9 @@
+//
+//  Profile.swift
+//  HashtagFinance
+//
+//  Created by Bondzio, Kevin M (bondz001) on 4/14/16.
+//  Copyright © 2016 Rhiannon Weiss. All rights reserved.
+//
+
+import Foundation
