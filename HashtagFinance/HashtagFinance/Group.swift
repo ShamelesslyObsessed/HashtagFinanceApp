d96a941929fa6990f83687fd3edb3ac900488cfd
Bondzio, Kevin M (bondz001) <bondz001@maclab-112.uwp.edu>
2016-04-19 16:38:20 -0500

    added code to group class

--- HashtagFinance/HashtagFinance/Group.swift
+++ HashtagFinance/HashtagFinance/Group.swift
@@ -9,3 +9,49 @@
 import UIKit
 
 // need to create a group object
+
+class Group: NSObject, NSCoding{
+    
+    var name: String!
+    var participants: [User]! = []
+    var balance: Double!
+    
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("group")
+    
+    // MARK: Types
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let participantsKey = "participants"
+        static let balanceKey = "balance"
+    }
+    
+    // MARK: Initialization
+    init?(name: String, balance: Double, founder: User) {
+        self.balance = balance
+        self.name = name
+        self.participants.append(founder)
+        
+        super.init()
+    }
+    
+    // MARK: NSCoding
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(balance, forKey: PropertyKey.balanceKey)
+        aCoder.encodeObject(participants, forKey: PropertyKey.participantsKey)
+        
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let balance = aDecoder.decodeObjectForKey(PropertyKey.balanceKey) as! Double
+        let participants = aDecoder.decodeObjectForKey(PropertyKey.participantsKey) as? [User]
+
+        
+        // Must call designated initializer.
+        self.init(name: name, balance: balance, founder: (participants?.first)!)
+    }
+
+}
